e420f8435ea7e5305414bfac94063e8e931b7c7a
David Châteauvert <davidchateauvert@MacBook-Pro-de-David.local>
2024-05-21 22:25:26 -0400

    Default is now english

--- lib/providers/locale_provider.dart
+++ lib/providers/locale_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LocaleProvider with ChangeNotifier {
-  Locale _locale = const Locale('fr');
+  Locale _locale = const Locale('en');
 
   Locale get locale => _locale;
 
